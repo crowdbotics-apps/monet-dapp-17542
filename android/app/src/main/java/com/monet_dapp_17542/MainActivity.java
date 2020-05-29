aa124b2e82d3ea0437d0e14359d40d9e061f8a7e
Crowdbotics <team@crowdbotics.com>
2020-05-29 14:25:44 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/monet_dapp_17542/MainActivity.java
@@ -0,0 +1,15 @@
+package com.monet_dapp_17542;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "monet_dapp_17542";
+    }
+}
