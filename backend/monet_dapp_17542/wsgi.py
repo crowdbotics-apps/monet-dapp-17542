a3829683b29108a3fff5430b3db70e2ff39061fe
Crowdbotics <team@crowdbotics.com>
2020-05-29 14:26:41 +0000

    Update models

--- backend/monet_dapp_17542/wsgi.py
+++ backend/monet_dapp_17542/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'monet_dapp_17542.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "monet_dapp_17542.settings")
 
 application = get_wsgi_application()
